dd6da92423e68f0badec924246753492276ca5b0
이승진 <dltmdwls154@gmail.com>
2023-11-03 16:23:33 +0900

    페이지 전환 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/next_screen.dart';
 
 void main() {
   runApp(const App());
@@ -12,7 +13,89 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    const asdf = 3;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'zxcvm,.vxcz.m,nm.,zxvc.vnxcm,',
+          style: TextStyle(fontSize: 14),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(0),
+        color: Color.fromARGB(255, 184, 184, 184),
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          children: <Widget>[
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+                'asdfkl;jasd;lfkjas;ldkfjas;ldkfjas;dlkfjas;dlkfjas;dlkfja'),
+            const Text('data sdfjksfadhhjlsfdljkasfd'),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return NextScreen();
+                  }));
+                },
+                child: Text('Go to next screen')),
+          ],
+        ),
       ),
     );
   }
