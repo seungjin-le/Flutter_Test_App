681a2185d2e8a3b480b9049d2df953079ef9f049
Lee Seung Jin <dltmdwls154@gmail.com>
2023-11-09 22:19:21 +0900

    Test Commit

--- lib/const/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const PRIMARY_COLOR = Color(0xFF0DB2B2);
-final LIGHT_GRAY_COLOR = Colors.grey[200];
-final DARK_GRAY_COLOR = Colors.grey[600];
-final TEXT_FIELD_GRAY_COLOR = Colors.grey[300];
