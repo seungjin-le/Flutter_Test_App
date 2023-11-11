2fe489aeeaf7e70a90d8ca2d38bc3995944ec74b
Lee Seung Jin <dltmdwls154@gmail.com>
2023-11-12 04:53:30 +0900

    bottom Widget 추가

---
+++ lib/widgets/bottom_bar_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+          activeIcon: Icon(Icons.book),
+        ),
+      ],
+    );
+  }
+}
